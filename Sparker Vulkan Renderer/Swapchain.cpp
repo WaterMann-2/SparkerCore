46d8505da1fb1f40003c7d8b8f638380eff0ec10
Watermann <102046675+WaterMann-2@users.noreply.github.com>
2025-01-16 21:24:18 -0600

    Delete Sparker Vulkan Renderer directory

--- Sparker Vulkan Renderer/Swapchain.cpp
+++
@@ -1,9 +0,0 @@
-#include "Swapchain.h"
-
-VkSwapchainKHR* Graphics::SVulkan::Swapchain::createSwapchain(VkDevice& srcDevice, VkPhysicalDevice& srcPhysicalDevice, SpWindow& window){
-	device = &srcDevice;
-	physicalDevice = &srcPhysicalDevice;
-	surface = window.getWindowSurface();
-
-	return &swapchain;
-}
